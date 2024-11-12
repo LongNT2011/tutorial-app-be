127c6744144ca947787b347735dd96223837c7aa
“longnt20” <longntk2@gmail.com>
2024-11-12 22:13:06 +0700

    update build spec

--- scripts/deploy_process.sh
+++ scripts/deploy_process.sh
@@ -7,18 +7,10 @@ SECRET=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --region $
 DOCKER_USERNAME=$(echo $SECRET | jq -r .DockerUsername)
 DOCKER_PASSWORD=$(echo $SECRET | jq -r .DockerPassword)
 
-echo "your-docker-password" | docker login -u $DockerUsername -p $DockerPassword"
+docker login -u $DockerUsername -p $DockerPassword"
+
+docker compose down || docker rmi -f $(docker images -q)
 
 docker rm -f $(docker ps -q -a)
 
-docker rmi -f $(docker images -q)
-
-sleep 3
-
-docker pull /aws-codepipeline-project:latest
-
-if [[ $? != 0 ]]; then
-    docker pull your-dockerhub-username/aws-codepipeline-project:latest
-fi
-
-docker run -d -p 80:3000 --name aws-codepipeline-project your-dockerhub-username/aws-codepipeline-project:latest
+docker compose up -d
